6be96f022bf9a077d60a61d6bd8bdff35a662506
Melo Ortega <melo-ortega@adabaed.com>
2022-09-15 18:22:04 +0100

    change: scheduler timing

--- server/scrapper/scrapperRoutine.ts
+++ server/scrapper/scrapperRoutine.ts
@@ -1,6 +1,6 @@
 import schedule from "node-schedule";
 import dailyScrapper from "./dailyScrapper";
 
-const scrapperJob = schedule.scheduleJob("00 */6 * * *", () => dailyScrapper());
+const scrapperJob = schedule.scheduleJob("30 */1 * * *", () => dailyScrapper());
 
 export default scrapperJob;
