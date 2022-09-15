c5532aceeb8f97bd6cd5d4bf81963e1e1e2bf69d
Carlos Rocholl <rocholl97@gmail.com>
2022-09-15 21:36:14 +0100

    0 2 * * * * * *

--- server/scrapper/scrapperRoutine.ts
+++ server/scrapper/scrapperRoutine.ts
@@ -1,6 +1,6 @@
 import schedule from "node-schedule";
 import dailyScrapper from "./dailyScrapper";
 
-const scrapperJob = schedule.scheduleJob("30 */1 * * *", () => dailyScrapper());
+const scrapperJob = schedule.scheduleJob("0 2 * * *", () => dailyScrapper());
 
 export default scrapperJob;
